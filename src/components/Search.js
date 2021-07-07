fdd086e5f0e232a2b230bd1e86b350e9f1381acb
Rattapon si <rattapon.simsri0@gmail.com>
2021-07-07 23:00:16 +0700

    import usestat

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,24 +1,23 @@
-import React from "react";
+import { useEffect, useState } from "react";
 
 // const pokamon_name = [{}];
 export const Search = (pokemon) => {
-  const [searchTerm, setSearchTerm] = React.useState("");
-  const [searchResults, setSearchResults] = React.useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
   const handleChange = (event) => {
     setSearchTerm(event.target.value);
   };
-  React.useEffect((pokemon) => {
-    const results = pokemon.filter(pokemon_name =>
-      pokemon_name.name.toLowerCase().includes(searchTerm)
-    );
-    setSearchResults(results);
-  }, [searchTerm]);
-console.log(pokemon)
+
+  console.log(pokemon);
   return (
     <form>
       Search Pokemon :
-      <input type="text" placeholder="Search..." value={searchTerm}
-        onChange={handleChange}/>
+      <input
+        type="text"
+        placeholder="Search..."
+        value={searchTerm}
+        onChange={handleChange}
+      />
     </form>
   );
 };
